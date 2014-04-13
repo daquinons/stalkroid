80701d833cb49a9218b3b8ec6f098f232edb5a97
David Lechón Quiñones <dalequi@gmail.com>
2014-04-13 19:42:27 +0200

    Visual changes, list and detail view with templates

--- stalker/views.py
+++ stalker/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from stalker.models import Asteroid
 
 def near(request):
     near_asteroids = Asteroid.objects.filter(next_to_earth=True)
 
     return render(request, 'near.html', {'near_asteroids': near_asteroids})
+
+class AsteroidDetailView(DetailView):
+    model = Asteroid
+
+    def get_context_data(self, **kwargs):
+        context = super(AsteroidDetailView, self).get_context_data(**kwargs)
+        return context
--- stalkroid/urls.py
+++ stalkroid/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from stalker.views import AsteroidDetailView
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$','stalker.views.near', name='near'),
-    # Examples:
-    # url(r'^$', 'stalkroid.views.home', name='home'),
-    # url(r'^stalkroid/', include('stalkroid.foo.urls')),
+    url(r'^$', 'stalker.views.near', name='near'),
+    url(r'^asteroid/(?P<slug>[A-Za-z0-9_\-]+)/$', AsteroidDetailView.as_view( template_name="asteroid.html"), name='asteroid_detail'),
+    url(r'^why/$', TemplateView.as_view(template_name = "why.html")),
+    url(r'^about/$', TemplateView.as_view(template_name = "about.html")),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
