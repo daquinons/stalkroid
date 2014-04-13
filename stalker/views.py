4f2bc06e3701089b24cc0caa313dc574768bf607
David Lechón Quiñones <dalequi@gmail.com>
2014-04-13 17:38:55 +0200

    Listing the near asteroids in the index page

--- stalker/views.py
+++ stalker/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from stalker.models import Asteroid
 
-# Create your views here.
+def near(request):
+    near_asteroids = Asteroid.objects.filter(next_to_earth=True)
+
+    return render(request, 'near.html', {'near_asteroids': near_asteroids})
--- stalkroid/urls.py
+++ stalkroid/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$','stalker.views.near', name='near'),
     # Examples:
     # url(r'^$', 'stalkroid.views.home', name='home'),
     # url(r'^stalkroid/', include('stalkroid.foo.urls')),
